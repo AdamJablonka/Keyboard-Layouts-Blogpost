e3cc12a406a73d37dc74145c03c112dee9f68fcb
AdamJablonka <ajjablonka@gmail.com>
2022-10-03 16:01:00 -0400

    added transition animations to clickable icons, visual changes on navbar and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { TbCode } from "react-icons/tb";
 
 const Footer = () => {
   return (
     <div
       id="full-size"
-      className=" bottom-0 mt-5 h-1/2 w-full flex justify-center bg-indigo-700"
+      className="gap-1 bottom-0 mt-5 h-1/2 w-full flex justify-center"
     >
-      <p>Adam Jablonka - React-Type</p>
+      <p className="m-1 text-l text-gray-400 select-none">
+        Adam Jablonka - React-Type
+      </p>
+      <a
+        className="p-0 m-0"
+        href="https://github.com/AdamJablonka/Keyboard-Layouts-Blogpost"
+      >
+        <p className="mt-0.5 text-gray-400 hover:text-white cursor-pointer transition hover:scale-105 duration-100 text-xl pt-1">
+          <TbCode />
+        </p>
+      </a>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,14 @@ import { BsKeyboard } from "react-icons/bs";
 // backdrop-blur-md
 const Navbar = () => {
   return (
-    <header className="fixed w-full p-2 z-0 backdrop-blur-md bg-gradient-to-r from-pink-500/60 to-indigo-500/30">
+    <header className="fixed w-full p-2 z-0 backdrop-blur-md bg-gradient-to-r from-black/50 to-white/30">
       <div className="mx-auto max-w-3xl flex flex-row">
-        <p className="font-sans p-2 font-bold select-none">React-Type Wiki</p>
+        <p className="font-sans text-2xl p-2 text-white font-bold select-none">
+          React-Type Wiki
+        </p>
         <a href="https://adamjablonka.github.io/React-Type/">
           <BsKeyboard
-            className="text-zinc-300 hover:text-white cursor-pointer text-2xl flex float-right mt-[6.5px]"
+            className="text-gray-200 transition ease-in-out hover:text-white hover:scale-105 duration-100 cursor-pointer text-4xl flex float-right mt-[6.5px]"
             onClick={() => console.log("Hello world!")}
           />
         </a>
