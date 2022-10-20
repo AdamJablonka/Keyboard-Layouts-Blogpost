623d4ae99e7de7d1dca9f002b145f7766fd583a1
AdamJablonka <ajjablonka@gmail.com>
2022-10-20 16:12:02 -0400

    Changes to styling, added mobile functionality for image slider

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -16,26 +16,38 @@ const ImageCarousel = () => {
   };
 
   const changePhotoLeft = () => {
-    console.warn(imageIndex);
     if (imageIndex < 1) {
       setImageIndex(images.length - 1);
     } else setImageIndex(imageIndex - 1);
   };
 
   return (
-    <div className="p-10 text-4xl flex col justify-center max-w-3xl">
-      <p className="m-auto hover:text-white cursor-pointer mr-5">
-        <BsArrowLeftSquare onClick={() => changePhotoLeft()} />
-      </p>
-      <div className="flex m-2 min-w-128 max-w-128 justify-center">
-        <img
-          src={images[imageIndex]}
-          className="rounded-xl object-contain flex md:w-96 md:h-48 transition ease-in-out delay-150 select-none"
-        ></img>
+    <div>
+      <div
+        id="wrapper"
+        className="p-10 h-96 text-4xl flex col justify-center max-w-3xl"
+      >
+        <p className="m-auto md:opacity-100 transition easy-in-out duration-100 hover:scale-105 hover:text-white cursor-pointer mr-2 display:none">
+          <BsArrowLeftSquare onClick={() => changePhotoLeft()} />
+        </p>
+        <div className="m-2 flex shrink-0 object-contain justify-center border border-indigo-500 rounded-xl w-full md:w-full">
+          <img
+            src={images[imageIndex]}
+            className="shrink-0 container m-5 p-3 rounded-md object-contain select-none"
+          ></img>
+        </div>
+        <p className="md:m-auto md:ml-2 md:opacity-100 transition easy-in-out duration-100 hover:scale-105 display:none hover:text-white cursor-pointer">
+          <BsArrowRightSquare onClick={() => changePhotoRight()} />
+        </p>
       </div>
-      <p className="m-auto hover:text-white cursor-pointer ml-5">
-        <BsArrowRightSquare onClick={() => changePhotoRight()} />
-      </p>
+      <div className="flex col justify-center text-4xl">
+        <p className="ml-5 md:opacity-0 opacity-100 hover:text-white cursor-pointer transition easy-in-out duration-100 hover:scale-105">
+          <BsArrowLeftSquare onClick={() => changePhotoRight()} />
+        </p>
+        <p className="ml-5 md:opacity-0 opacity-100 hover:text-white cursor-pointer transition easy-in-out duration-100 hover:scale-105">
+          <BsArrowRightSquare onClick={() => changePhotoRight()} />
+        </p>
+      </div>
     </div>
   );
 };
